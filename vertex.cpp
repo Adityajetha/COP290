6c777db066d658f9a5ac38141a03d14b4900cf38
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-02 19:31:23 +0530

    Update vertex.cpp

--- vertex.cpp
+++ vertex.cpp
@@ -3,12 +3,12 @@
 
 public class vertex{
                     public double x,y,z;
-                    public rotat(double aq,double bq,double cq)
+                    public rotat(double aq,double bq,double cq) ///To rotate the vertex for 3D to 2D projection.
                     {
-                        double d = sqrt(aq*aq+bq*bq+cq*cq);
-                        double a = a/d;
-                        double b = b/d;
-                        double c = c/d;
+                        double d = sqrt(aq*aq+bq*bq+cq*cq);  ///Normalisation constant
+                        double a = aq/d;                     ///(a,b,c) is unit vector along the perpendicular to the normal
+                        double b = bq/d;
+                        double c = cq/d;
                         double e = sqrt(b*b + c*c);
                         x = (e*e*x - a*b*y -a*c*z)/e;
                         y = (c*y/e) - (b*z/e);
