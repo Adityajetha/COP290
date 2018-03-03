2e2acc32ddd551c0db53b9dedc1f99bd616c0670
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-03 13:18:32 +0530

    Update and rename okay.cpp to xy.cpp

--- okay.cpp
+++
@@ -1,4 +0,0 @@
-#include "vertex.h"
-#include <stdio.h>
-
-int main 
---
+++ xy.cpp
@@ -0,0 +1,7 @@
+#include "vertex.h"
+#include <stdio.h>
+
+void xy (vertex v, double x, double y){
+         v.x = x;
+         v.y = y;
+}
