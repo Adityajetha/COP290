f6439d511932f4776f13ea435571be7d1c00ac89
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-04 17:05:38 +0530

    Create two3.cpp

---
+++ two3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "vertex.h"
+
+class vertex{}
+
+public class two3{
+             int num;
+             vertex ver[] = ver[num];
+             for each vertex
+             {
+                 
+             }
