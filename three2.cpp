6fe29b8fb12b3700a5d058d55d31534bfb603830
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-04 15:04:23 +0530

    Create three2.cpp

---
+++ three2.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "vertex.h"
+
+class vertex{}
+
+public class three2 {
+             int num,a,b,c;
+             vertex ver[] = new ver[num];
+             bool edge[][] = new bool[num][num];
+             
+             void vertexinp(vertex[] v){};
+             void edgeinp(bool[][] e){};
+             void planeinp(int* a,int* b,int* c){};
+             public static void main(string[] args){
+             vertexinp(ver);
+             edgeinp(edge);
+             }
+             
+                    }
+                    
