1b3fd16a76041873454e00eb0486428797daf9f1
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-04 17:19:03 +0530

    Update two3.cpp

--- two3.cpp
+++ two3.cpp
@@ -5,8 +5,19 @@ class vertex{}
 
 public class two3{
              int num;
+             bool valid,opn;
              vertex ver[] = ver[num];
+             public static void main(){
+             cin>>opn;
              for each vertex
              {
-                 
+               xy();
+               valid=valid && yz();
+               valid=valid && xz();
              }
+  if(opn)
+    createedge();
+  else
+    edgelist();
+             }
+}
