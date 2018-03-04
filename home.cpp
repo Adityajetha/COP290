956fe42a3158656c6469eb364b8fcbeec1524c34
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-04 14:02:30 +0530

    Create home.cpp

---
+++ home.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "three2.h"
+#include "two3.h"
+
+public class home{
+             bool is3;
+             public void three2(){}
+             public void two3(){}
+             static void main(string args[]){
+             cin>>is3
+             if(is3)
+             three2();
+             else
+             two3();
+             }
