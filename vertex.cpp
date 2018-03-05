6666949d6816ef2b8001d7b817808b9e64f19b2d
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 09:02:48 +0530

    Update vertex.cpp

--- vertex.cpp
+++ vertex.cpp
@@ -2,16 +2,14 @@
 #include <math.h>
 #include "vertex.h"
 
-public class vertex{
-                    public double x,y,z;
-                    public rotat(double aq,double bq,double cq) ///To rotate the vertex for 3D to 2D projection.
-                    {
-                        double d = sqrt(aq*aq+bq*bq+cq*cq);  ///Normalisation constant
-                        double a = aq/d;                     ///(a,b,c) is unit vector along the perpendicular to the normal
-                        double b = bq/d;
-                        double c = cq/d;
-                        double e = sqrt(b*b + c*c);
-                        x = (e*e*x - a*b*y -a*c*z)/e;
-                        y = (c*y/e) - (b*z/e);
-                        z = 0;
-                    }
+vertex::vertex(double xt,double yt,double zt)
+{
+  x(xt);
+  y(yt);
+  z(Zt);
+}
+
+vertex::rotat(double aq,double bq,double cq)
+{
+  ///To rotate the vertex
+}
