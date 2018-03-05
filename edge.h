df46d01792460aec307a0cb1372cf44bfceac8af
Riya Singh <32234233+riyasars@users.noreply.github.com>
2018-03-05 12:27:36 +0530

    Update edge.h

--- edge.h
+++ edge.h
@@ -1,4 +1,5 @@
 class edge{
                     public: edge(vertex ver_1, vertex ver_2, bool[][] adj_matrix);
-                    public: double ver_1,ver_2;                       ///ver_1,ver_2 coordinates of vertex
+                    public: vertex ver_1,ver_2;                       ///ver_1,ver_2 coordinates of vertex
+                    public : bool[][] adj_matrix; // adjacency matrix
              }; 
