9531e6462366e73b07306baa7f8a6349298a838d
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 15:00:36 +0530

    Update two3.h

--- two3.h
+++ two3.h
@@ -4,14 +4,14 @@
 #include <bits/stdc++.h>
 
 public class two3{
-             int num;///num is the number of vertices
-             bool[][] adj_matrix///represents edges
-             bool valid;///valid checks is the given orthographic projections are correct
-             bool opn;///opn selects whether edges are given or not 
-             vertex ver[] = new vertex[num];///array of all the vertices
-             std::vector<face> faces;///Vector of the faces
+             public: int num;///num is the number of vertices
+             public: bool[][] adj_matrix///represents edges
+             public: bool valid;///valid checks is the given orthographic projections are correct
+             public: bool opn;///opn selects whether edges are given or not 
+             public: vertex ver[] = new vertex[num];///array of all the vertices
+             public: std::vector<face> faces;///Vector of the faces
              public: static void main();
-             std::vector<face> conversion(vertex ver,bool[][] adj_matrix); ///makes all the faces
-             bool[][] findedgeloops(bool[][] adj);///removes impossible edges
-             void create_3D(bool[][] edge,std::vector<face> faces);///final step 
+             public: std::vector<face> conversion(vertex ver,bool[][] adj_matrix); ///makes all the faces
+             public: bool[][] findedgeloops(bool[][] adj);///removes impossible edges
+             public: void create_3D(bool[][] edge,std::vector<face> faces);///final step 
 }
