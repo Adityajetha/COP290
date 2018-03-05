21e4929fb38cce1d0892861d365d032fb706b9fb
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 12:24:35 +0530

    Update two3.h

--- two3.h
+++ two3.h
@@ -6,10 +6,10 @@
 
 public class two3{
              int num;///num is the number of vertices
-             bool adj_matrix = new bool[num][num];///represents edges
+             bool adj_matrix///represents edges
              bool valid;///valid checks is the given orthographic projections are correct
              bool opn;///opn selects whether edges are given or not 
              vertex ver[] = ver[num];///array of all the vertices
              public: static void main()
-             void conversion(ver, adj_matrix); ///makes all the faces
+             void conversion(vertex ver,bool[][] adj_matrix); ///makes all the faces
 }
