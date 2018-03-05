494da4867d80780bae5203dedac1506e4bee5b73
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 15:13:03 +0530

    Update two3.h

--- two3.h
+++ two3.h
@@ -14,6 +14,9 @@ public class two3{
              public: bool opn;///opn selects whether edges are given or not 
              public: vertex ver[] = new vertex[num];///array of all the vertices
              public: std::vector<face> faces;///Vector of the faces
+             public: void xy (vertex v, double x, double y);///takes input from top view
+             public: bool xz (vertex v, double z, double x);///takes input from side view
+             public: bool yz (vertex v, double y, double z);/// takes input from front view
              public: static void main();
              public: std::vector<face> conversion(vertex ver,bool[][] adj_matrix); ///makes all the faces
              public: bool[][] findedgeloops(bool[][] adj);///removes impossible edges
