931aa77d7947289ef35dc1a7f4a5c51bfc86e81c
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 16:06:16 +0530

    Update yz.cpp

--- yz.cpp
+++ yz.cpp
@@ -1,6 +1,6 @@
 #include "vertex.h"
 #include <stdio.h>
-#include "setcoor.h"
+#include "two3.h"
 
 bool yz (vertex v, double z, double y){
          ///to check for y coordinate and set the z coordinate
