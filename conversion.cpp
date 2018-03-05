a688451166bf812ebaafd963f8d71d6a648a0a81
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 13:49:53 +0530

    Update conversion.cpp

--- conversion.cpp
+++ conversion.cpp
@@ -1,22 +1,10 @@
-#include <stdio.h>
 #include"findedgeloops.h"
 #include"create_3D.h"
 #include"makeface.h"
+#include <bits/stdc++.h>
 
-conversion(vertex[],bool[][]){
+void conversion (vertex[] ver,bool[][] edges){
   int num_common_vertex;
   bool[][] adj_matrix;
-for each common vertex 
-{
-  makeface(); // returns face array containing all possible faces formed by edges 
-  int num_face;
-  face face_list[] = new face[num_face];
-  
-}
-  for each face
-  {
-    findedgeloops(adj_matrix[][]); 
-  }
-  
-  create_3D(adj_matrix[][],face_list[]);  
+  std::vector<face> faces;
 }
