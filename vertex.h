d01e23b0ae0949344581bc549e69cc78dc9576b2
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 08:52:01 +0530

    Create vertex.h

---
+++ vertex.h
@@ -0,0 +1,4 @@
+class vertex{
+                    public: double x,y,z;                       ///x,y,z, coordinates of vertex
+                    public: void rotat(double aq,double bq,double cq) ///To rotate the vertex for 3D to 2D projection.
+             };       
