e5a976001550a620ada95947b78e325f8e99b4bf
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 10:12:51 +0530

    Update createedge.cpp

--- createedge.cpp
+++ createedge.cpp
@@ -1,1 +1,12 @@
 #include <stdio.h>
+
+void createedge(bool[][] edge)
+{
+  int num;///number of vertices
+  bool[][] top = new bool[num][num];///edges in top view
+  bool[][] front = new bool[num][num];///edges in front view
+  bool[][] side = new bool[num][num];///edges in side view
+  getedge(top);///gets the edges in top view
+  getedge(front);///gets the edges in front view
+  getedge(side);///gets the edges in side view
+}
