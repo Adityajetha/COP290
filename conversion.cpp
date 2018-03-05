efd045c234c6baef37bc7b66c7a3ab9fb9cb0c44
Riya Singh <32234233+riyasars@users.noreply.github.com>
2018-03-05 09:39:21 +0530

    Update conversion.cpp

--- conversion.cpp
+++ conversion.cpp
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+conversion(ver[],adj_matrix[][]){
+  int num_common_vertex;
+for each common vertex 
+{
+  makeface(); // returns face array containing all possible faces formed by edges 
+}
+  for each face
+  {
+    findedgeloops(); 
+    for all edge loop 
+    {
+      adj_matrix[ver_1][ver_2] = false;
+    }
+  }
+  
+  
+}
