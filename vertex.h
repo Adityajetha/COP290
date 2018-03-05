c31e31a6a20c1af15e674360d1fe74ed98152e52
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 09:03:23 +0530

    Update vertex.h

--- vertex.h
+++ vertex.h
@@ -1,5 +1,5 @@
 class vertex{
-                    public: vertex(double x, double y, double z);
+                    public: vertex(double xt, double yt, double zt);
                     public: double x,y,z;                       ///x,y,z, coordinates of vertex
                     public: void rotat(double aq,double bq,double cq); ///To rotate the vertex for 3D to 2D projection.
              };       
