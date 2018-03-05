429959cf0e0eea42a975abe494e30a1cbfb2e497
Riya Singh <32234233+riyasars@users.noreply.github.com>
2018-03-05 12:33:49 +0530

    Update and rename face.cpp to face.h

---
+++ face.h
@@ -0,0 +1,5 @@
+class face {
+  public: face(int num_of_edge, vector<edge> edge_list, bool hidden);
+                    public: vector<edge> edge_list;                       ///list of edges
+                    public : bool hidden; // hidden face or not
+}
