fb04a0664df209a9a0aa100b56e73210c904da42
Riya Singh <32234233+riyasars@users.noreply.github.com>
2018-03-05 13:13:11 +0530

    Update findedgeloop.cpp

--- findedgeloop.cpp
+++ findedgeloop.cpp
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include<vector>
 #include<edge.h>
-vector<edge> findedgeloop() 
+
+findedgeloop(bool[n][n]) 
 {
-  num_of_edges;
-  loop_edge = new vector<edge>;
-  for all edge e
+  struct bool checkLoop(int a, int b){   
+  };
+  int n;
+  adj_matrix[n][n] = bool [n][n];
+  for all edge 
   {
-   if checkLoop(e)  
-   {loop_edge.assign(e);}
+   if (checkLoop(i,j) ) 
+   {
+      adj_matrix[i][j] = false; 
+   }
     else 
     {continue;}
     //checks if its an edge loop or not
   }
-   // fill in all edges that are found to be looped
-  return loop_edge;
 }
 
