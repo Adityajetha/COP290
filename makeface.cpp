759db3261a255e5ce5fe228b6644abcbbb24c0e9
Riya Singh <32234233+riyasars@users.noreply.github.com>
2018-03-05 12:46:29 +0530

    Update makeface.cpp

--- makeface.cpp
+++ makeface.cpp
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<vertex.h>
+makeface() 
+{
+  bool[][] adj_matrix;
+  vertex[] ver;
 
+  graph = makeGraph(adj_matrix); //makes graph with every vertex as a node and every corresponding edge added 
+  for every common vertex 
+  {
+    checkLoop(graph); //implemented using dfs search of the graph   
+  }    
+  
+}  
