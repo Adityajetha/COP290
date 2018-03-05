b2b465568e4462bc753c85b5e04a0edc54bd7ca4
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 12:03:00 +0530

    Update three2.cpp

--- three2.cpp
+++ three2.cpp
@@ -3,30 +3,22 @@
 #include "three2.h"
 
 
-public class three2 {
-             int num ////num is the number of vertices
-             int a,b,c; ////a,b,c are respectively the x,y and z coefficients of the direction vector perpendicular to the
-                        ////plane we want to take the projection about
-             vertex ver[] = new vertex[num];
-                        ///aray of vertices
-             bool edge[][] = new bool[num][num];
-                        ///edge[i][j] is true if edge exists between the vertices i,j
-             void vertexinp(vertex[] v)
+             three2::void vertexinp(vertex[] v)
              {
                ///Fuction to take inputs of all the vertices
              }
-             void edgeinp(bool[][] e)
+             three2::void edgeinp(bool[][] e)
              {
               ///Fuction to take all the edges as input 
              }
-             void planeinp(int* a,int* b,int* c)
+             three2::void planeinp(int* a,int* b,int* c)
              {
                ///Function to take the plane as input we want to draw the projection along
              }
-             public static void main(string[] args){
+             three2::public: static void main(string[] args){
              vertexinp(ver);
              edgeinp(edge);
              }
              
-                    }
+                    
                     
