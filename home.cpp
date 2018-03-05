58186709079a20b66d3df235418ac2fafa5ec6a8
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 08:20:49 +0530

    Update home.cpp

--- home.cpp
+++ home.cpp
@@ -3,9 +3,13 @@
 #include "two3.h"
 
 public class home{
+             ////Home page of the software
              bool is3;
+             ////is3 decides whether the software will be used for 2D to 3D reconstruction or from 3D to 2D Projection
              public void three2(){}
+             ////funtion for 3D to 2D
              public void two3(){}
+             ////function for 3D to 2D
              static void main(string args[]){
              cin>>is3
              if(is3)
