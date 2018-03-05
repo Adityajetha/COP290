d6d1e577a9a1c2426f102d501e56a2bdbd0b9fd3
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 16:13:16 +0530

    Update two3.h

--- two3.h
+++ two3.h
@@ -13,7 +13,7 @@ public class two3{
              public: bool valid;///valid checks is the given orthographic projections are correct
              public: bool opn;///opn selects whether edges are given or not 
              public: vertex ver[] = new vertex[num];///array of all the vertices
-             public: std::vector<face> faces;///Vector of the faces
+             public: std::vector<face> faces;///Vector of all the faces possible(not all real)
              public: void xy (vertex v, double x, double y);///takes input from top view
              public: bool xz (vertex v, double z, double x);///takes input from side view
              public: bool yz (vertex v, double y, double z);/// takes input from front view
