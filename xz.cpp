55eedbfa08877cb12b59f16289cc398b956c1d88
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 16:06:35 +0530

    Update xz.cpp

--- xz.cpp
+++ xz.cpp
@@ -1,6 +1,6 @@
 #include "vertex.h"
 #include <stdio.h>
-#include "setcoor.h"
+#include "two3.h"
 
 bool xz (vertex v, double z, double x){
          //to check if the x and z coordinates match in all views.
