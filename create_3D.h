4524bbbb28b824735e8442417133ee8fc4b1f3d6
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 15:02:24 +0530

    Update create_3D.h

--- create_3D.h
+++ create_3D.h
@@ -1,19 +1,20 @@
 #include<stdio.h>
 #include<face.h>
+#include <bits/stdc++.h>
 public struct create_3D {
 
 public : int num; ////num is the number of vertices
              public : bool[][] adj_matrix; ////adj_matrixis the adjacency matrix
              public : vertex ver[] = new vertex[num];
                         ///aray of vertices
-             public : vector<face> face_list = new vector<face>;
+             public : std::vector<face> face_list = new std::vector<face>;
                         
              public : int  n,num_vertex;
      
              public :bool checkFace(vector<face> face_list,bool[][] adj_matrix)
-             {
+             
                ///checks if a face is real or not
-             }
-             }
+             
+             
              
                     }
