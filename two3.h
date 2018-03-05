1f0f978802bb441cee19f784aa77f8ce69bbde0e
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-05 12:06:45 +0530

    Create two3.h

---
+++ two3.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "vertex.h"
+#include "createedge.h"
+#include "edgelist.h"
+#include "conversion.h"
+
+public class two3{
+             int num;///num is the number of vertices
+             bool adj_matrix = new bool[num][num];///represents edges
+             bool valid;///valid checks is the given orthographic projections are correct
+             bool opn;///opn selects whether edges are given or not 
+             vertex ver[] = ver[num];///array of all the vertices
+             public: static void main()
+             conversion(ver, adj_matrix); ///makes all the faces
+}
