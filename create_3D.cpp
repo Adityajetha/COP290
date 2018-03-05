430c51e1019d331b256213efbf5e6e1398cbb2da
Riya Singh <32234233+riyasars@users.noreply.github.com>
2018-03-05 13:38:40 +0530

    Update create_3D.cpp

--- create_3D.cpp
+++ create_3D.cpp
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include<vector>
 
+
+create_3D(bool[n][n],vertex[num_vertex])
+{
+  adj_matrix = bool[n][n];
+   vector<face> face_list;
+  int n,num_vertex;
+  checkFace(face_list, adj_matrix){}
+  
+}
