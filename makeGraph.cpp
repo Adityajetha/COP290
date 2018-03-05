9cd5835820de5100699a9c865eb132334ece4f53
Riya Singh <32234233+riyasars@users.noreply.github.com>
2018-03-05 11:51:19 +0530

    Create makeGraph.cpp

---
+++ makeGraph.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+makeGraph()
+{
+  bool[][] adj_matrix;
+  struct AdjListNode
+  {
+  vertex dest;
+  struct AdjListNode* next;
+  }
+  struct Graph* createGraph(vertex V){}
+  struct AddEdge(vertex src, vertex des, struct Graph* graph){}
+  
+  
+}
