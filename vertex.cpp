d3f1e2150c312970bcab2601cc0bdb66e78d2d4c
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-11 16:34:55 +0530

    Update vertex.cpp

--- vertex.cpp
+++ vertex.cpp
@@ -4,12 +4,20 @@
 
 vertex::vertex(double xt,double yt,double zt)
 {
-  x(xt);
-  y(yt);
-  z(zt);
+    x(xt);
+    y(yt);
+    z(zt);
 }
 
 vertex::rotat(double aq,double bq,double cq)
 {
-  ///To rotate the vertex
+    double d = sqrt(aq*aq+bq*bq+cq*cq);  ///Normalisation constant
+    double a = aq/d;                     ///(a,b,c) is unit vector along the perpendicular to the normal
+    double b = bq/d;
+    double c = cq/d;
+    double e = sqrt(b*b + c*c);
+    x = (e*e*x - a*b*y -a*c*z)/e;
+    y = (c*y/e) - (b*z/e);
+    z = 0;
+    ///To rotate the vertex
 }
