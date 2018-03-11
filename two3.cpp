5029d68c806f6ca8c8f964171ea9814c11ed41ca
Riya Singh <32234233+riyasars@users.noreply.github.com>
2018-03-11 23:06:13 +0530

    Update two3.cpp

--- two3.cpp
+++ two3.cpp
@@ -1,7 +1,25 @@
-//
-// Created by Aditya Jetha on 10-03-2018.
-//
+#include <stdio.h>
+#include "vertex.h"
+#include "createedge.h"
+#include "edgelist.h"
+#include "two3.h"
 
+ two3::public: static void main(){
+             /*
+             for each vertex
+             {
+               xy();
+               valid=valid && yz();
+               valid=valid && xz();
+             }           
+  if(!opn)
+    createedge(edge);///input does not contain the edges
+  else
+    edgelist(edge);///input contains the edges
+                         ///for each edge edge[ver_1][ver_2] is true
+             */}
+  
+  two3::conversion(ver, edge); ///makes all the faces
 #include <stdio.h>
 #include "vertex.h"
 #include "createedge.h"
@@ -13,6 +31,10 @@ class two3 {
 public: bool opn;
 public: int num;
 public: vertex ver[];
+public: std::vector<face> facelist;
+public: std::vector<face> :: iterator iter;
+public: std::vector<face> :: reverse_iterator iter_r;
+two3: int num_faces;
 public:
     static void main(char *args[]) {
         std::cin >> num >> std::end;
@@ -37,15 +59,73 @@ public:
         }
 
         if (!two3::getopn)
-            createedge(adj_matrix);///input does not contain the edges
+            createedge(edge);///input does not contain the edges
         else
-            edgelist(adj_matrix);///input contains the edges
-        ///for each edge adj_matrix[ver_1][ver_2] is true
+            edgelist(edge);///input contains the edges
+        ///for each edge edge[ver_1][ver_2] is true
     }
+
+two3 :: void detectEdge(){
+  //How to incorporate hidden lines and edges
+  two3:: int num = sizeof(ver)/sizeof(ver[0]); 
+  public::bool[][] edge = new bool[num][num]; //Final adjacency matrix.
+  //xy_edge, yz_edge, zx_edge
+  for(int i =0; i< num; i++){
+    for (int j=0; j< num; j++){
+      edge[i][j] = xy_edge[i][j] * yz_edge[i][j] * zx_edge[i][j]; //checking if it is 1 in all the adjacency matrix
+    }
+  }
+}
+
+//ver[] is the vertex array and edge[][] is the adjacency matrix. So, we have detected the vertices and the edges
+
+two3 :: void findedgeloop(){
+  //finds the edgehoops
+  bool[][] edge;
+  vertex[] ver;
+  sort(ver, ver + num); //sorts the vertices, but check if it will.
+
+
+}
+
+two3 :: void makeface(){
+  std::vector<face> facelist;
+  std::vector<face> :: iterator iter;
+  std::vector<face> :: reverse_iterator iter_r;
+  int num_faces = facelist.size();
+  //finds the faces constructed using the actual edge list and the vertices
+
+}
+two3 :: void edgecount(Edge e){ //counts how many times it appears in the vector consisting of faces
+
+
+}
+
+two3:: void checkface(){ 
+
+  //distinguishes between the real and pseudo elements
+  //face is a vector of elements of the face type
+  /*1 : When an edge is adjacent to more than two faces, at most two faces can be true.*/
+  for(int i =0; i< num; i++){
+    for (int j=0; j< num; j++){
+      if (edge[i][j]==1) {
+        for (int k =0; k < num_faces; k++){
+          
+        }       
+      }
+    }
+  }
+
+}
+
+two3:: void isometricview(){
+  //produce using 3D description an isometric drawing from any view direction
+  /////
+}
 
 public: void conversion(vertex ver[],bool edges[][]){
         int num_common_vertex;
-        bool adj_matrix[][];
+        bool edge[][];
         std::vector<face> faces;
     } ///makes all the faces
 };
