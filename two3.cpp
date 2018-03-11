b0f1da8400549eae41dee420aa057d68f4be7e68
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-11 16:32:22 +0530

    Update two3.cpp

--- two3.cpp
+++ two3.cpp
@@ -1,22 +1,51 @@
+//
+// Created by Aditya Jetha on 10-03-2018.
+//
+
 #include <stdio.h>
 #include "vertex.h"
 #include "createedge.h"
 #include "edgelist.h"
 #include "two3.h"
 
- two3::public: static void main(){
-             /*
-             for each vertex
-             {
-               xy();
-               valid=valid && yz();
-               valid=valid && xz();
-             }           
-  if(!opn)
-    createedge(adj_matrix);///input does not contain the edges
-  else
-    edgelist(adj_matrix);///input contains the edges
-                         ///for each edge adj_matrix[ver_1][ver_2] is true
-             */}
-  
-  two3::conversion(ver, adj_matrix); ///makes all the faces
+class two3 {
+
+public: bool opn;
+public: int num;
+public: vertex ver[];
+public:
+    static void main(char *args[]) {
+        std::cin >> num >> std::end;
+        double temp1,temp2;
+        for (int i = 0; i < num; i++) {
+            std::cin>>ver[i].x>>ver[i].y>>std::end;
+        }
+        for (int i = 0; i < num; i++) {
+            std::cin>>temp1>>ver[i].z>>std::end;
+            if(temp!=ver[i].y){
+                ///error
+            }
+        }
+        for (int i = 0; i < num; i++) {
+            std::cin >> temp1 >> temp2 >> std::end;
+            if (temp1 != ver[i].x && temp2 != ver[i].z) {
+                ///error
+            }
+        }
+        for (int i = 0; i < num; i++) {
+            xz();
+        }
+
+        if (!two3::getopn)
+            createedge(adj_matrix);///input does not contain the edges
+        else
+            edgelist(adj_matrix);///input contains the edges
+        ///for each edge adj_matrix[ver_1][ver_2] is true
+    }
+
+public: void conversion(vertex ver[],bool edges[][]){
+        int num_common_vertex;
+        bool adj_matrix[][];
+        std::vector<face> faces;
+    } ///makes all the faces
+};
