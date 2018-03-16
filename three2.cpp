6d452809e14f6e610e8ec45bfe3488104115d702
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-16 20:29:04 +0530

    Update three2.cpp

--- three2.cpp
+++ three2.cpp
@@ -2,13 +2,9 @@
 #include <string.h>
 #include "vertex.h"
 #include "three2.h"
-three2::num=4;
-three2::edge = new bool[num][num];
-three2::ver = new vertex[num];
-three2::edge;
-three2::vertexinp()
+void three2::vertexinp()
 {
-    std::cin>>num;
+    cin>>num;
     ///Fuction to take inputs of all the vertices
     ver = new vertex [num];
     for(int i =0;i<num; i++)
@@ -18,7 +14,7 @@ three2::vertexinp()
         std::cin>>ver[i].z>>std::end;
     }
 }
-three2::edgeinp()
+void three2::edgeinp()
 {
     edge=new bool[num][num];
     for(int i = 0;i<num;i++)
@@ -31,14 +27,19 @@ three2::edgeinp()
 
     ///Fuction to take all the edges as input
 }
-three2::planeinp(double* a,double* b,double* c)
+void three2::planeinp(double* a,double* b,double* c)
 {
     cin>>*a;
     cin>>*b;
     cin>>*c;
     ///Function to take the plane as input we want to draw the projection along
 }
-three2::main(char* args){
+
+void three2::createedge(bool edge[][])
+{
+}
+
+three2::main(int argc,char* args[]){
 
     vertexinp();
     edgeinp();
