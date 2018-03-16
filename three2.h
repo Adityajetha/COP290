44d511fc1631fe619933ff21f01b37fd0a6ab586
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-16 20:31:52 +0530

    Update three2.h

--- three2.h
+++ three2.h
@@ -14,15 +14,15 @@ public class three2 {
                         ///aray of vertices
              public : bool edge[][] = new bool[num][num];
                         ///edge[i][j] is true if edge exists between the vertices i,j
-             public : void createedge(bool edge[][])
+             public : void createedge(bool edge[][]);
                         ////dekhta hu
-             public : void vertexinp(vertex[] v)
+             public : void vertexinp(vertex[] v);
              
                ///Fuction to take inputs of all the vertices
-             public : void edgeinp(bool e[][])
+             public : void edgeinp(bool e[][]);
               ///Fuction to take all the edges as input 
              
-             public : void planeinp(int* a,int* b,int* c)
+             public : void planeinp(int* a,int* b,int* c);
              
                ///Function to take the plane as input we want to draw the projection alone
              }
