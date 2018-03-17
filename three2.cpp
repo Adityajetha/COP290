67ec35593cdc84dea7730b440b9d58a4a61ab1c5
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-17 18:23:32 +0530

    Update three2.cpp

--- three2.cpp
+++ three2.cpp
@@ -1,12 +1,66 @@
 #include <iostream>
 #include <string.h>
 #include "vertex.h"
-#include "three2.h"
+#include "three.h"
+#include "edge.h"
+#include "vector.h"
+
+bool edge::iscov(vertex v, vector<vertex> plane) {
+    int n = plane.size();
+    bool is = false;
+    vertex one = new vertex(plane[0].x,plane[0].y,plane[0].z);
+    vertex two = new vertex(plane[1].x,plane[1].y,plane[1].z);
+    vertex three = new vertex(plane[2].x,plane[2].y,plane[2].z);
+    one = one.sub(two,one);
+    two = two.sub(three,two);
+    
+}
+
+
+
+bool edge::che_hid(vertex v){
+  
+}
+void edge::create(){
+  ord=ver;
+   int i, j;
+  vertex key = new vertex(0,0,0);
+   for (i = 1; i < n; i++)
+   {
+       key = ord[i];
+       j = i-1;
+ 
+       /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+       while (j >= 0 && ord[j].z > key.z)
+       {
+           ord[j+1] = ord[j];
+           j = j-1;
+       }
+       ord[j+1] = key;
+   }
+  for(i=0;i<num;i++){
+    hid[i]=che_hid(ver[i]);
+  }
+  for(i=0;i<num;i++){
+    for(j=0;j<num;j++){
+      if(hid[i]&&hid[j])
+        solid[i][j]=0;
+      else if((!hid[i])&&(!hid[j]))
+        solid[i][j]=1;
+      else if(hid[i]&&!hid[j])
+        solid[i][j]=2;
+      else
+        solid[i][j]=3;
+  }
+  }
+}
 
 void three::three(int n){
-                         three::num=n;
-                         three::edge = new bool[n][n];
-                        three::ver = new vertex[n]
+                         num=n;
+                         edge = new bool[n][n];
+                        ver = new vertex[n]
                         }
 void three::vertexinp()
 {
