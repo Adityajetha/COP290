1a64ffa29098263716d9320d12f5b8d6ca87e86d
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-17 15:14:31 +0530

    Update edge.h

--- edge.h
+++ edge.h
@@ -1,5 +1,11 @@
+#include "vertex.h"
+
 class edge{
-                    public: edge(vertex ver_1, vertex ver_2, bool[][] adj_matrix);
-                    public: vertex ver_1,ver_2;                       ///ver_1,ver_2 coordinates of vertex
-                    public : bool[][] adj_matrix; // adjacency matrix
+                    public: edge(vertex vert[], bool adj_matri[][]);
+                    public: vertex ver[];                       ///ver_1,ver_2 coordinates of vertex
+                    public : bool adj_matrix[][]; /// adjacency matrix
+                    public: vertex ord[];
+                    public: bool solid[][];
+                    public : bool che_hid(vertex v);///checks if vertex is hidden or not
+                    public : void create();
              }; 
