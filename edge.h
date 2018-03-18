42597c7955984a28eafdda325646007bde029e43
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-18 09:53:53 +0530

    Update edge.h

--- edge.h
+++ edge.h
@@ -6,6 +6,8 @@ class edge{
                     public: vertex ver[];                       ///ver_1,ver_2 coordinates of vertex
                     public : bool adj_matrix[][]; /// adjacency matrix
                     public: vertex ord[];
+  public: vector<vector<vertex>> faces;
+  public: vector<vector<vertex>> facelist;
                     public: int solid[][];
                     public: bool iscov(vertex v, vector<vertex> plane);
                     public : bool che_hid(vertex v);///checks if vertex is hidden or not
