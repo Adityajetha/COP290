a1c5e03b42e19d81fd1a0acd48977a05cc44a9f0
Adityajetha <aditya_jetha@yahoo.co.in>
2018-03-19 02:19:04 +0530

    Update edge.h

--- edge.h
+++ edge.h
@@ -3,11 +3,11 @@
 #include <vector>
 
 class edge{
+  public: int num;
                     public: edge(string file);
-                    public: vertex ver[];                       ///ver_1,ver_2 coordinates of vertex
-                    public : bool adj_matrix[][]; /// adjacency matrix
-                    public: vertex ord[];
-  public: vector<vector<vertex>> faces;
+                    public: vertex* ver[num];                       ///ver_1,ver_2 coordinates of vertex
+                    public : bool adj_matrix[num][num]; /// adjacency matrix
+                    public: vertex* ord[num];
   public: vector<vector<vertex>> facelist;
                     public: int solid[][];
                     public: bool iscov(vertex v, vector<vertex> plane);
