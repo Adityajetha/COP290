df5aa41ea2fa3900d794fc2109e7576ba623e45d
Riya Singh <32234233+riyasars@users.noreply.github.com>
2018-04-01 19:04:54 +0530

    Delete face.h

--- face.h
+++
@@ -1,5 +0,0 @@
-class face {
-  public: face(int num_of_edge, vector<edge> edge_list, bool hidden);
-                    public: vector<edge> edge_list;                       ///list of edges
-                    public : bool hidden; // hidden face or not
-}
