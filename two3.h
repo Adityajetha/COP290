16c48fdce32d92780bac98c2363404fcc7b33b22
Adityajetha <aditya_jetha@yahoo.co.in>
2018-04-05 14:57:26 +0530

    Update two3.h

--- two3.h
+++ two3.h
@@ -1,45 +1,32 @@
-#include <GL/glui.h>
-#include <OpenGL/gl.h>
-#include <OpenGl/glu.h>
-#include <GLUT/glut.h>
-#include <stdio.h>
-#include "vertex.h"
-#include "face.h"
-#include <bits/stdc++.h>
+#ifndef GTKMM_TWO3_H
+#define GTKMM_TWO3_H
 
-public class two3{
-             ///num is the number of vertices
-             public: int num;
-             ///represents edges
-             public: bool[][] adj_matrix;
-             ///valid checks is the given orthographic projections are correct
-             public: bool valid;
-             ///opn selects whether edges are given or not
-             public: bool opn; 
-             ///array of all the vertices
-             public: vertex ver[] = new vertex[num];
-             ///Vector of all the faces possible(not all real)
-             public: std::vector<face> faces;
-             ///takes input from top view
-             public: std::vector<face> makeface(int num,vertex[] ver,bool[][] edge); 
-             public : void createedge(bool[][] edge);
-                        ////dekhta hu
-             public: void xy (vertex v, double x, double y);
-             ///takes input from side view
-             public: bool xz (vertex v, double z, double x);
-             /// takes input from front view
-             public: bool yz (vertex v, double y, double z);
-             ///it takes input from all the views, when opn=0, and gives their intersection
-             public: void createedge(bool[][] edge);
-             ///it takes inputs from the users to give the edges, in case opn =1
-             public: void edgelist(bool[][] edge);
-             public: static void main();
-             ///makes all the faces
-             public: std::vector<face> makeface(int num,vertex[] ver,bool[][] edge);
-             ///makes wireframe
-             public: std::vector<face> conversion(vertex ver,bool[][] adj_matrix);
-             ///removes impossible edges
-             public: bool[][] findedgeloops(bool[][] adj);
-             ///final step
-             public: void create_3D(bool[][] edge,std::vector<face> faces); 
-}
+#include <gtkmm.h>
+#include <string.h>
+#include "main.h"
+
+class Two3 : public Gtk::Window
+{
+public:
+  Two3();
+  virtual ~Two3();
+
+protected:
+  //Signal handlers:
+  void on_checkbox_editable_toggled();
+  void on_checkbox_visibility_toggled();
+  void on_button_close();
+  void topview();
+  void frontview();
+  void sideview();
+  std::string str1,str2,str3;
+
+  //Child widgets:
+  Gtk::Box m_HBox;
+  Gtk::Box m_VBox;
+  Gtk::Button m_Entryx,m_Entryy,m_Entryz;
+  Gtk::Button m_Button_Close;
+  Gtk::Button m_CheckButton_Editable, m_CheckButton_Visible;
+};
+
+#endif //GTKMM_EXAMPLEWINDOW_H
